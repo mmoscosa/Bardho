73339163dea4b4ef8676636da2d288727876fa35
Martin Moscosa <mmoscosa@Martins-MacBook-Air.local>
2017-11-05 13:28:52 -0600

    Issue #6 Users can subscribe to mailing list as well as a confirmation mail with its code 😊

--- components/utils/Loading.vue
+++ components/utils/Loading.vue
@@ -1,5 +1,7 @@
 <template lang="html">
-  <div class="ui active inverted dimmer"><div class="ui indeterminate text loader" v-lang.index.loading></div></div>
+  <div class="ui active inverted dimmer">
+    <div class="ui indeterminate text loader" v-lang.index.loading></div>
+  </div>
 </template>
 
 <script>
