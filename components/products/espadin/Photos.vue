5963abe1f7888b67e2cb4b99f8d804e0187366c5
Martin Moscosa <mmoscosa@Martins-MacBook-Air.local>
2017-12-16 19:09:03 -0600

    nice photos on products page 🤪

--- components/products/espadin/Photos.vue
+++ components/products/espadin/Photos.vue
@@ -1,7 +1,8 @@
 <template lang="html">
   <viewer class="ui four column grid" :images="photos">
-    <div class="column" v-for="photo in photos">
-      <img :src="photo.images[0].source" :key="photo.name" class="ui bordered rounded image">
+    <div class="column" v-for="(photo, index) in photos">
+      <img :src="photo.images[0].source" :key="photo.name" class="ui bordered rounded image" v-if="index < 4">
+      <img :src="photo.images[0].source" :key="photo.name" class="ui bordered rounded image hidden" v-if="index > 4">
     </div>
   </viewer>
 </template>
@@ -21,7 +22,7 @@ export default {
     getPhotos: function () {
       var api = 'https://2oby6oykv0.execute-api.us-west-2.amazonaws.com/bardho/fbgraph'
       axios.post(api, {
-        query: '785825174941072?fields=photos.limit(4){link,images,name}'
+        query: '785825174941072?fields=photos{link,images,name}'
       }).then((response) => {
         if (response) {
           this.photos = response.data.photos.data
