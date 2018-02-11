33053ac08e286c545ccbfbb295434a5a064656eb
Martin Moscosa <mmoscosa@Martins-MacBook-Air.local>
2018-02-10 18:17:30 -0600

    ortogragia 😄

--- plugins/lang/es.js
+++ plugins/lang/es.js
@@ -1,6 +1,6 @@
 export default {
   'index': {
-    'blurb': 'Ya sea que estes cerrando un negocio, comenzando tu empresa, puede ser que te promovieron, no se hable de un desamor o un compromiso; muy al estilo mexicano el celebrar las victorias así como las derrotas, inequivocadamente el acompañante perfecto, aquel testigo es Mezcal Bard<span class="h">h</span>o. Para personas que siempre están buscando dar mas, arreglarselas con creatividad, siempre manteniendo un estilo muy propio, personas que nunca se dan por vencido.',
+    'blurb': 'Ya sea que estés cerrando un negocio, comenzando tu empresa, puede ser que te promovieron, no se hable de un desamor o un compromiso; muy al estilo mexicano el celebrar las victorias así como las derrotas, inequivocadamente el acompañante perfecto; aquel testigo es Mezcal Bard<span class="h">h</span>o. Para personas que siempre están buscando dar más, arreglárselas con creatividad, siempre manteniendo un estilo muy propio, personas que nunca se dan por vencidas.',
     'slogan': 'Compartiendo historias'
   },
   'about': {},
@@ -8,18 +8,18 @@ export default {
     'espadin': {
       'name': 'Espadín',
       'price': '$380.00 MXN',
-      'promo': '<strong>Comparte y Gana</strong>, si 10 personas se inscriben con tu código, participa para ganar <strong>una de las primeras 10 botellas</strong> de Mezcal Bard<span class="h">h</span>o. No esperes más, regitrate y obtén tu código para participar.',
+      'promo': '<strong>Comparte y Gana</strong>, si 10 personas se inscriben con tu código, participa para ganar <strong>una de las primeras 10 botellas</strong> de Mezcal Bard<span class="h">h</span>o. No esperes más, regístrate y obtén tu código para participar.',
       'alcohol': '40% Vol. Alc.',
       'version': 'v0.1',
       'volume': '750 ml',
       'description': {
         'title': 'Descripción',
         'shortDescription': 'De sabor y aroma ahumado, característica de su cocimiento, su cristalinidad transmite la pureza y la honestidad con la que ha sido elaborado dejando en el paladar el auténtico sabor del mezcal oaxaqueño.',
-        'longDescription': 'De sabor y aroma ahumado, característica de su cocimiento, su cristalinidad transmite la pureza y la honestidad con la que ha sido elaborado dejando en el paladar el auténtico sabor del mezcal oaxaqueño.'
+        'longDescription': 'De sabor y aroma ahumado, característica de su cocimiento, su cristalinidad transmite la pureza y la honestidad con la que ha sido elaborado, dejando en el paladar el auténtico sabor del mezcal oaxaqueño.'
       },
       'master': {
         'title': 'Maestro Mezcalero',
-        'bio': 'José pertenece a la tercera generación de maestros mezcaleros. José se mantiene al tanto de cada uno de los procesos de elaboración, desde la siembra de los magueyes hasta la comercialización de la bebida; además destaca que para hacer buen mezcal debe haber un fuerte vínculo entre el productor y la tierra.',
+        'bio': 'José pertenece a la tercera generación de maestros mezcaleros. José se mantiene al tanto de cada uno de los procesos de elaboración, desde la siembra de los magueyes hasta la comercialización de la bebida; además destaca que para hacer buen mezcal, debe haber un fuerte vínculo entre el productor y la tierra.',
         'name': 'José Augusto Hernández Escobar',
         'picture': 'https://firebasestorage.googleapis.com/v0/b/mezcal-bardho.appspot.com/o/master.gif?alt=media&token=137a4787-8688-4aa6-9b2a-4752a346f059'
       },
@@ -42,7 +42,7 @@ export default {
   },
   'contact': {
     'title': 'Contacto',
-    'blurb': '¿Tienes dudas o comentarios? En Bard<span class=\'h\'>h</span>o nos encanta escuchar a quienes comparten esa ideologia de siempre crecer. Siempre abiertos a nuevas ideas asi como oportunidades.',
+    'blurb': '¿Tienes dudas o comentarios? En Bard<span class=\'h\'>h</span>o nos encanta escuchar a quienes comparten esa ideología de siempre crecer. Siempre abiertos a nuevas ideas así como oportunidades.',
     'form': {
       'button': 'Enviar',
       'mail': 'Correo electrónico',
@@ -50,7 +50,7 @@ export default {
       'name': 'Nombre'
     },
     'sidebar': {
-      'title': 'Siguenos en redes sociales   ',
+      'title': 'Síguenos en redes sociales   ',
       'sharing': 'Comparte Bard<span class="h">h</span>o con tus amistades',
       'blurb': ''
     }
@@ -67,19 +67,19 @@ export default {
       }
     },
     'mailList': {
-      'title': 'Registrate',
-      'blurb': 'Si aun no te registras a nuestra lista, te invitamos a que te registres a para recibir las ultimas noticias de Bard<span class="h">h</span>o, asi como acceso exclusivo a preventas.',
-      'spam': 'Lo detestamos tanto como tu, prometemos no SPAM 😇',
+      'title': 'Regístrate',
+      'blurb': 'Si aún no te registras a nuestra lista, te invitamos a que lo hagas para recibir las últimas noticias de Bard<span class="h">h</span>o, así como acceso exclusivo a preventas.',
+      'spam': 'Lo detestamos tanto como tú, prometemos no SPAM 😇',
       'form': {
         'email': 'Correo Electrónico',
         'name': 'Nombre Completo',
-        'referal': '¿Codigo de invitación?',
+        'referal': '¿Código de invitación?',
         'button': 'Unirse'
       }
     },
     'thanksMailingList': {
       'title': '¡Felicidades!',
-      'blurb': 'Ya estas registrado en nuestra base, estas muy cerca de ganar tu primera botella de Bard<span class="h">h</span>o',
+      'blurb': 'Ya estás registrado en nuestra base; estás muy cerca de ganar tu primera botella de Bard<span class="h">h</span>o',
       'steps': {
         'one': {
           'title': 'Registrarse',
@@ -88,14 +88,14 @@ export default {
         'two': {
           'title': 'Compartir',
           'description': 'Invita a tus amistades',
-          'code': 'Tu codigo:'
+          'code': 'Tu código:'
         },
         'three': {
           'title': '¡Gana!',
           'description': 'Disfruta la historia de Bard<span class="h">h</span>o'
         }
       },
-      'share': 'Te invitamos a que compartas en las diferentes redes sociales tu link personalisado para que más personas se registren con tu codigo, dandote asi mayor oportunidad de ganar; no esperes más, son solo un par de clicks.'
+      'share': 'Te invitamos a que compartas en las diferentes redes sociales tu link personalizado para que más personas se registren con tu código, dándote así mayor oportunidad de ganar. No esperes más, son sólo un par de clicks.'
     },
     'loading': {
       'message': 'Cargando'
@@ -106,7 +106,7 @@ export default {
     'sharesocial': {},
     'disclaimers': {
       'cookies': 'Usamos cookies 🍪 en nuestro sitio para ofrecer un mejor servicio <a class="ui mini inverted basic button" href="/disclaimers?section=cookies">Más información</a>',
-      'age': 'Debes tener al menos 18 años de edad 🔞 <small> - o la edad minima en tu pais</small> <a class="ui mini inverted basic button" href="/disclaimers?section=privacy">Más información - Aviso de Privacidad</a>',
+      'age': 'Debes tener al menos 18 años de edad 🔞 <small> - o la edad mínima en tu país</small> <a class="ui mini inverted basic button" href="/disclaimers?section=privacy">Más información - Aviso de Privacidad</a>',
       'menu': {
         'cookies': 'Cookies',
         'privacy': 'Aviso de Privacidad'
